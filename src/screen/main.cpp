9dc98c0edbb9aa181a313bc2077e514d9ce497ff
ehzx <edrickhong@gmail.com>
2020-11-05 08:46:53 +0800

    new hello world program. seems to be working

--- src/screen/main.cpp
+++ src/screen/main.cpp
@@ -1,6 +1,8 @@
 #include "WProgram.h"
 #include "ttype.h"
 
+#define LED_BUILTIN 13
+
 extern "C" void __cxa_pure_virtual(void)
 {
 	// call to a pure virtual function happened ... wow, should never happen ... stop
@@ -18,14 +20,14 @@ s32 main(s32 argc,s8** argv){
 	init();
 
 	Serial.begin(9600);
-	pinMode(13,OUTPUT);
+	pinMode(LED_BUILTIN,OUTPUT);
 
 	s32 state = HIGH;
 
 	while(1){
 
 		Serial.println("Hello world");
-		digitalWrite(13,state);
+		digitalWrite(LED_BUILTIN,state);
 
 		(state == HIGH) ? (state = LOW) : (state = HIGH);
 		delay(1000);
